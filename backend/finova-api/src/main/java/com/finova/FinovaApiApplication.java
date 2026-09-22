5d43c0d9722d63f089afcb48051cda3a09929f87
sc-raw <dakeamv@gmail.com>
2026-09-22 15:05:18 -0500

    Finova - Inicio y base del proyecto

---
+++ backend/finova-api/src/main/java/com/finova/FinovaApiApplication.java
@@ -0,0 +1,13 @@
+package com.finova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinovaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinovaApiApplication.class, args);
+	}
+
+}
---
+++ backend/finova-api/src/test/java/com/finova/FinovaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinovaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
